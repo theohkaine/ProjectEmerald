2910e40726d8e84b5424168c1106f47b245fda6e
MrPoutine <zachy24@hotmail.com>
2019-12-21 18:04:22 -0500

    Gun & Shooting Mechanics
    -Custom Gun added
    -weaponHold Position in character hand
    -added shooting of LaserShots(bullet prefab)

--- ProjectEmerald/Assets/Scripts/Player.cs
+++ ProjectEmerald/Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(PlayerController))]
+[RequireComponent(typeof(GunController))]
 public class Player : MonoBehaviour
 {
 
@@ -10,21 +11,24 @@ public class Player : MonoBehaviour
 
     Camera viewCamera;
     PlayerController controller;
-    
+    GunController gunController;
 
     void Start()
     {
         controller = GetComponent<PlayerController>();
+        gunController = GetComponent<GunController>();
         viewCamera = Camera.main;
     }
 
     
     void Update()
     {
+        //Movement Input
         Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         Vector3 moveVelocity = moveInput.normalized * moveSpeed;
         controller.Move(moveVelocity);
 
+        //Look Input
         Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
         Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
         
@@ -36,6 +40,12 @@ public class Player : MonoBehaviour
             controller.LookAt(point);
 
             Debug.DrawLine(ray.origin, point, Color.red);   //FOR DEBUGGING PURPOSES
+        }
+
+        //Weapon Input
+        if (Input.GetMouseButton(0))
+        {
+            gunController.Shoot();
         }
 
     }
---
+++ ProjectEmerald/Assets/Scripts/Projectile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    float speed = 10;
+
+    public void SetSpeed(float newSpeed)
+    {
+        speed = newSpeed;
+    }
+    void Update()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * speed);
+    }
+}
